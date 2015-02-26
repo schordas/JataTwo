ebbfef188ab568987c8345a65937a24409960dcf
Greg Berger <grberger@usc.edu>
2015-02-26 12:42:34 -0800

    Front end refactoring. Yes, I know this is the wrong branch, but you'll GIT over it

--- client/templates/bar_graph.js
+++ client/templates/bar_graph.js
@@ -1,56 +1,72 @@
 if(Meteor.isClient) {
 
-  Template.barGraph.rendered = function () {
+  //   Data.find().observe({ 
+  //   added: function(post) {
+  //     console.log('Added to Data');
+  //   },
+  //   changed: function(post) {
+  //     console.log('Change to Data');
+  //   },
+  //   removed: function(post) {
+  //     console.log('Removed from Data');
+  //   }
+  // });
 
-    var margin = {top: 20, right: 30, bottom: 30, left: 40},
-      width = 960 - margin.left - margin.right,
-      height = 500 - margin.top - margin.bottom;
+  Deps.autorun(
+     function () {
 
-    var x = d3.scale.ordinal()
-        .rangeRoundBands([0, width], .1);
+        var margin = {top: 20, right: 30, bottom: 30, left: 40},
+          width = 960 - margin.left - margin.right,
+          height = 500 - margin.top - margin.bottom;
 
-    var y = d3.scale.linear()
-        .range([height, 0]);
+        var x = d3.scale.ordinal()
+            .rangeRoundBands([0, width], .1);
 
-    var xAxis = d3.svg.axis()
-        .scale(x)
-        .orient("bottom");
+        var y = d3.scale.linear()
+            .range([height, 0]);
 
-    var yAxis = d3.svg.axis()
-        .scale(y)
-        .orient("left");
+        var xAxis = d3.svg.axis()
+            .scale(x)
+            .orient("bottom");
 
-    var chart = d3.select(".chart")
-        .attr("width", width + margin.left + margin.right)
-        .attr("height", height + margin.top + margin.bottom)
-      .append("g")
-        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+        var yAxis = d3.svg.axis()
+            .scale(y)
+            .orient("left");
 
-    d3.tsv("data.tsv", type, function(error, data) {
-      x.domain(data.map(function(d) { return d.name; }));
-      y.domain([0, d3.max(data, function(d) { return d.value; })]);
+        var chart = d3.select(".chart")
+            .attr("width", width + margin.left + margin.right)
+            .attr("height", height + margin.top + margin.bottom)
+          .append("g")
+            .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-      chart.append("g")
-          .attr("class", "x axis")
-          .attr("transform", "translate(0," + height + ")")
-          .call(xAxis);
 
-      chart.append("g")
-          .attr("class", "y axis")
-          .call(yAxis);
+            console.log(Data.find().count() + " entries");
+        d3.json(Data.find(), type, function(error, data) {
+          x.domain(data.map(function(d) { return d.name; }));
+          y.domain([0, d3.max(data, function(d) { return d.value; })]);
 
-      chart.selectAll(".bar")
-          .data(data)
-        .enter().append("rect")
-          .attr("class", "bar")
-          .attr("x", function(d) { return x(d.letter); })
-          .attr("y", function(d) { return y(d.frequency); })
-          .attr("height", function(d) { return height - y(d.value); })
-          .attr("width", x.rangeBand());
-    });
+          chart.append("g")
+              .attr("class", "x axis")
+              .attr("transform", "translate(0," + height + ")")
+              .call(xAxis);
+
+          chart.append("g")
+              .attr("class", "y axis")
+              .call(yAxis);
+
+          chart.selectAll(".bar")
+              .data(data)
+            .enter().append("rect")
+              .attr("class", "bar")
+              .attr("x", function(d) { return x(d.letter); })
+              .attr("y", function(d) { return y(d.frequency); })
+              .attr("height", function(d) { return height - y(d.value); })
+              .attr("width", x.rangeBand());
+        }); 
 
 
   }//rendered()
+  );
 
 }//isClient()
 

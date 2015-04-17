5a3729fd23610578ab8e3a9e16a6269115777bef
Samuel Chordas <samec39@gmail.com>
2015-04-17 16:42:20 -0700

    fixed args

--- parser.py
+++ parser.py
@@ -9,7 +9,7 @@ if fileName[(len(fileName) - 4):] == '.csv':
 	jsonFile = open( fileName[:-4] + '.json','w')
 	# Change each fieldname to the appropriate field name. I know, so difficult.
 	fieldNames = ("Expenditure Type","Fiscal Year","Period Nbr","Project Number","Task Cognizant Org","Task Number","MTD Burdened Costs","MTD Actual FTE","MTD Hours","MTD Burdened Obligations","MTD Obligations","MTD EAC BurdenedPlan","MTD EAC Raw Plan","MTD EOC Burdened Plan","MTD EOC Raw Plan","MTD EAC FTE Plan","MTD EAC Hours Plan","MTD Burdened Cost Plan","MTD Burdened Oblg Plan","MTD FTE Plan","MTD Hours Plan","MTD Raw Cost Plan","MTD Raw Oblg Plan")
-
+    
 	reader = csv.DictReader(csvFile, fieldNames)
 	count = 0
 	#Parse the CSV into JSON
